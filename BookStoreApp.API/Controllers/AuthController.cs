769d6d3f9ca0d9d511c95ece9811d3f1da6107c8
Billy <billyrobshaw2k3@gmail.com>
2025-12-31 14:11:02 +0000

    User Login and Register API Set up

---
+++ BookStoreApp.API/Controllers/AuthController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using BookStoreApp.API.Data;
+using BookStoreApp.API.Models.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly ILogger<AuthController> logger;
+        private readonly UserManager<ApiUser> userManager;
+        public AuthController(IMapper mapper, ILogger<AuthController> logger, UserManager<ApiUser> userManager)
+        {
+            this.logger = logger;
+            this.mapper = mapper;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(UserDto userDto)
+        {
+            logger.LogInformation($"User Registration attempt for {userDto.Email}");
+            var user = mapper.Map<ApiUser>(userDto);
+            user.UserName = userDto.Email;
+            var result  = await userManager.CreateAsync(user, userDto.Password);
+            try
+            {
+                if (result.Succeeded == false)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    return BadRequest(ModelState);
+                }
+
+                await userManager.AddToRoleAsync(user, "User");
+
+                return Accepted();
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, $"Something went wrong in the {nameof(Register)}");
+                return Problem($"Something went wrong in the {nameof(Register)}", statusCode: 500);
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginUserDto userDto)
+        {
+            logger.LogInformation($"User Login attempt for {userDto.Email}");
+
+            try
+            {
+                var user = await userManager.FindByEmailAsync(userDto.Email);
+                var passwordValid = await userManager.CheckPasswordAsync(user, userDto.Password);
+
+                if (user == null || passwordValid == false)
+                {
+                    return NotFound();
+                }
+
+                return Accepted();
+
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, $"Something went wrong in the {nameof(Register)}");
+                return Problem($"Something went wrong in the {nameof(Register)}", statusCode: 500);
+            }
+        }
+
+    }
+}
